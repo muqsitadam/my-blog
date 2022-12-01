1edfcef6267f7dfb652f09332f27fa29aa62bb14
muqsitadam <muqsitadam@gmail.com>
2022-12-01 14:40:41 +0100

    Mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
     return(
     <div className="App bg-slate-200 h-screen">
         <div className='flex flex-col justify-center items-center'>
-            <div className='w-3/5 text-lg p-5'>
+            <div className='w-full xl:w-3/5 2xl:w-1/2  text-lg p-5'>
                 <h1 className="text-3xl font-medium">Hello, Welcome to my blog</h1>
                 <p className='mb-5'>Here are my posts and articles on different tech concepts I am opportuned to write on:</p>
                 <div>
@@ -28,10 +28,10 @@ const App = () => {
                     {!loading &&
                     posts.map((post,id) => {
                         return (
-                            <div className="flex my-5 justify-center border border-slate-800 rounded-lg" key={id}>
-                                <img className="w-1/3 object-fill rounded-l-lg" src={post.cover_image} alt="Cover image of article"/>
-                                <div className="w-2/3 h-60 px-4 py-2">                                
-                                    <div className="text-black font-bold text-3xl mb-1">{post.title}</div>
+                            <div className="flex flex-col lg:h-48 xl:h-60 2xl:h-48 lg:flex-row my-5 justify-center border border-slate-800 rounded-lg" key={id}>
+                                <img className="w-full lg:w-1/3 object-fill rounded-l-lg" src={post.cover_image} alt="Cover image of article"/>
+                                <div className="w-full lg:w-2/3 h-70 lg:h-60 px-4 pt-2 pb-5 lg:pb-10">                                
+                                    <div className="text-black font-bold text-2xl md:text-3xl mb-1">{post.title}</div>
                                     <p className="text-sm text-grey-dark flex items-center font-semibold">
                                     {post.published_at.slice(0, 10)}
                                     </p>
